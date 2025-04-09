5b2f341e6134c94615f9166bc638ddc6fa1d713e
haerinCS <123025165+crazycat948@users.noreply.github.com>
2025-04-09 00:50:20 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+       
+?>
+<!DOCTYPE html>
+<html lang ='en'>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href = "style.css">
+    <title>Meme Evolution Tracker</title>
+</head>
+<body>
+    <header class = "Header">
+        <h1 class = "Header_left">
+            Meme Evolution Tracker 😎
+        </h1>
+        <ul class = "Header_right">
+            <li><a href = "index.php">Search Page</a></li>
+            <li><a href = "upload.php">Upload Page</a></li>
+        </ul>
+        
+    </header>
+    <main>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia eligendi pariatur quisquam fugit excepturi perferendis est quae rerum aspernatur sint nisi, ipsam minus iusto, quia quo officiis? Error, incidunt veniam!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum architecto minima harum quam nulla, veritatis, error commodi dolores at debitis animi nostrum corrupti qui doloribus fugit laborum nesciunt dignissimos?</p>
+    </main>
+    <footer class = "Footer">
+    <div class = "info">
+            <h3 class = "info_topic">Team Members</h3>
+            <ul class = "members">
+                <li>Zainuddin Mohammed</li>
+                <li>Yifan Ren</li>
+                <li>Se Pdrer</li>
+                <li>Saol Tesfaghebriel</li>
+                <li>Junaid Bashi</li>
+            </ul>
+
+        </div>
+    </footer>
+    
+    <form action="index.php" method="post">
+        
+    </form><br>
+</body>
+</html>
+
+<?php
+       
+?>
